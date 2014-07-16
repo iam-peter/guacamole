14deb56d0c8cfb71814d36952118f92bbddc2040
Joshua Reibert <joshua.reibert@uni-weimar.de>
2014-07-16 16:27:06 +0200

    add handling of negative values

--- src/gua/renderer/AreaChartRessource.cpp
+++ src/gua/renderer/AreaChartRessource.cpp
@@ -52,10 +52,29 @@ AreaChartRessource::AreaChartRessource(
   , upload_mutex_()
   , num_indices_(0)
   , num_points_(0)
-  , xdata_(xdata)
-  , ydata_(ydata) {
+  , xdata_()
+  , ydata_() {
 
-  num_points_ = std::min(xdata_.size(), ydata_.size());
+  num_points_ = std::min(xdata.size(), ydata.size());
+
+  // add first value pair
+  xdata_.push_back(xdata[0]);
+  ydata_.push_back(ydata[0]);
+
+  // for all following pairs: check if there was a zero-crossing
+  // and eventually insert new points at the zero-crossings
+  for (unsigned int i(1); i < num_points_; ++i)
+  {
+    if ( (ydata[i - 1] < 0.0f) != (ydata[i] < 0.0f) )
+    {
+      float slope = (ydata[i] - ydata[i - 1]) / (xdata[i] - xdata[i - 1]);
+      float delta_x = -ydata[i - 1] / slope;
+      xdata_.push_back(xdata[i - 1] + delta_x);
+      ydata_.push_back(0.0f);
+    }
+    xdata_.push_back(xdata[i]);
+    ydata_.push_back(ydata[i]);
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -75,43 +94,40 @@ void AreaChartRessource::upload_to(RenderContext const& ctx) const
   vertices_[ctx.id] =
       ctx.render_device->create_buffer(scm::gl::BIND_VERTEX_BUFFER,
                                        scm::gl::USAGE_STATIC_DRAW,
-                                       num_points_ * 2 * sizeof(Vertex),
+                                       xdata_.size() * 2 * sizeof(Vertex),
                                        0);
 
   // set point vertices
   Vertex* data(static_cast<Vertex*>(ctx.render_context->map_buffer(
       vertices_[ctx.id], scm::gl::ACCESS_WRITE_INVALIDATE_BUFFER)));
 
-  // retrieve min and max values to center the area chart
-  float ymin = *std::min_element(ydata_.begin(), ydata_.end());
-  float xmin = *std::min_element(xdata_.begin(), xdata_.end());
-  float ymax = *std::max_element(ydata_.begin(), ydata_.end());
-  float xmax = *std::max_element(xdata_.begin(), xdata_.end());
-
-  float base = -0.5f * (ymax - ymin) + std::min(0.0f, ymin);
-
   // create triangles for every segment
-  for (unsigned int i(0); i < num_points_; ++i)
+  for (unsigned int i(0); i < xdata_.size(); ++i)
   {
-    float x = -0.5f * (xmax - xmin) + xdata_[i];
-    float y = -0.5f * (ymax - ymin) + ydata_[i];
+    scm::math::vec3f data_pos = scm::math::vec3f(xdata_[i], ydata_[i], 0.0f);
+    scm::math::vec3f anchor_pos = scm::math::vec3f(xdata_[i], 0.0f, 0.0f);
+    scm::math::vec3f normal = scm::math::vec3f(0.0f, 0.0f, 1.0f);
 
+    if (ydata_[i] >= 0.0f)
+    {
+      data[i * 2 + 0].pos = data_pos;
+      data[i * 2 + 1].pos = anchor_pos;
+    } else
+    {
+      data[i * 2 + 0].pos = anchor_pos;
+      data[i * 2 + 1].pos = data_pos;
+    }
 
-    // actual data point position
-    data[i * 2].pos = scm::math::vec3f(x, y, 0.0f);
-    data[i * 2].normal = scm::math::vec3f(0.0f, 0.0f, 1.0f);
-
-    // data point at baseline height
-    data[i * 2 + 1].pos = scm::math::vec3f(x, base, 0.0f);
-    data[i * 2 + 1].normal = scm::math::vec3f(0.0f, 0.0f, 1.0f);
+    data[i * 2 + 0].normal = normal;
+    data[i * 2 + 1].normal = normal;
   }
 
   ctx.render_context->unmap_buffer(vertices_[ctx.id]);
 
   // for each line segment 4 faces with each 6 vertices
-  std::vector<unsigned> index_array(num_points_ * 2);
+  std::vector<unsigned> index_array(xdata_.size() * 2);
 
-  for (unsigned int i(0); i < num_points_; ++i)
+  for (unsigned int i(0); i < xdata_.size(); ++i)
   {
     index_array[i * 2] = i * 2;
     index_array[i * 2 + 1] = i * 2 + 1;
@@ -120,7 +136,7 @@ void AreaChartRessource::upload_to(RenderContext const& ctx) const
   indices_[ctx.id] =
       ctx.render_device->create_buffer(scm::gl::BIND_INDEX_BUFFER,
                                        scm::gl::USAGE_STATIC_DRAW,
-                                       num_points_ * 2 * sizeof(unsigned),
+                                       xdata_.size() * 2 * sizeof(unsigned),
                                        &index_array[0]);
 
   // set point vertex array
@@ -156,7 +172,7 @@ void AreaChartRessource::draw(RenderContext const& ctx) const {
     ctx.render_context->bind_index_buffer(
         indices_[ctx.id], scm::gl::PRIMITIVE_TRIANGLE_STRIP, scm::gl::TYPE_UINT);
     ctx.render_context->apply();
-    ctx.render_context->draw_elements(num_points_ * 2);
+    ctx.render_context->draw_elements(xdata_.size() * 2);
   }
 }
 
